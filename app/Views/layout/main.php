8602641379c869e7ce96752a2a1f9fe49a73e9a5
Kennedy0-o <023100889d@uandina.edu.pe>
2026-09-09 10:40:12 -0500

    Fix: restauracion de clases Bootstrap y diseño del navbar principal

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -16,15 +16,20 @@
     </style>
 </head>
 <body>
-<a class="navbar-brand fw-bold" href="<?= base_url('index.php/pedidos') ?>">Sagras Restaurant</a>
-<div class="collapse navbar-collapse">
-    <ul class="navbar-link navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('index.php/pedidos') ?>">Pedidos</a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('index.php/pedidos/nuevo') ?>">Nuevo Pedido</a></li>
-        <li class="nav-item"><a class="nav-link" href="<?= base_url('index.php/productos') ?>">Productos</a></li>
-    </ul>
-</div>
-  </div>
+<nav class="navbar navbar-expand-lg navbar-dark navbar-sagras mb-4">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="<?= base_url('index.php/pedidos') ?>">Sagras Restaurant</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('index.php/pedidos') ?>">Pedidos</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('index.php/pedidos/nuevo') ?>">Nuevo Pedido</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?= base_url('index.php/productos') ?>">Productos</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
 <div class="container">
     <?php if (session()->getFlashdata('success')): ?>
